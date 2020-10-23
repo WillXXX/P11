4a64c49eafdfa5b149ffa078fd385ce388b95edf
Will <will.marcx@gmail.com>
2020-10-22 22:42:45 -0300

    AdapterTwo

--- app/src/main/java/com/example/p11/AdapterTwo.kt
+++ app/src/main/java/com/example/p11/AdapterTwo.kt
@@ -12,8 +12,16 @@ import androidx.recyclerview.widget.RecyclerView
 
 class AdapterTwo (val paginaWeb: PaginaWeb) : RecyclerviewTwo.AdapterTwo<GetViewHolder>(){
 
+    override fun onCreateViewhHolder(parent: ViewGroup): GetViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.activity_recyclerview_two, parent, false)
+    }
+
     override fun onBindViewHolder(holder: GetViewHolder,position: Int){
         val items = paginaWeb.items.get(position)
     }
 
+    override fun getItemcoun(): Int {
+        return paginaWeb.items.count()
+    }
 }
