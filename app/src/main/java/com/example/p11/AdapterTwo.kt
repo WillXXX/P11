8808bf1085370245e588f07dfd58805df6555763
Will <will.marcx@gmail.com>
2020-10-22 22:13:56 -0300

    *Updates

--- app/src/main/java/com/example/p11/AdapterTwo.kt
+++ app/src/main/java/com/example/p11/AdapterTwo.kt
@@ -1,2 +1,19 @@
 package com.example.p11
 
+import android.content.Intent
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.recyclerview.widget.RecyclerView
+
+
+class AdapterTwo (val paginaWeb: PaginaWeb) : RecyclerviewTwo.AdapterTwo<GetViewHolder>(){
+
+    override fun onBindViewHolder(holder: GetViewHolder,position: Int){
+        val items = paginaWeb.items.get(position)
+    }
+
+}
--- app/src/main/java/com/example/p11/RecyclerviewTwo.kt
+++ app/src/main/java/com/example/p11/RecyclerviewTwo.kt
@@ -2,8 +2,21 @@ package com.example.p11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 
 class RecyclerviewTwo : AppCompatActivity() {
+    open class AdapterTwo<T> {
+
+        open fun getItemcoun(): Int {}
+        open fun onCreateViewhHolder(parent: ViewGroup): GetViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        open fun onBindViewHolde(holder: GetViewHolder, position: Int) {}
+        open fun onBindViewHolder() {}
+        open fun onBindViewHolder(holder: GetViewHolder, position: Int) {}
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recyclerview_two)
