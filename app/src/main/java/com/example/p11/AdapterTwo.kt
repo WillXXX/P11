3266cb74d5a1bb2522a43453a03e9ed7145bb363
Will <will.marcx@gmail.com>
2020-10-25 23:54:12 -0300

    AdapterTwo

--- app/src/main/java/com/example/p11/AdapterTwo.kt
+++ app/src/main/java/com/example/p11/AdapterTwo.kt
@@ -13,18 +13,5 @@ import kotlinx.android.synthetic.main.activity_screen_two.view.*
 
 class AdapterTwo (val paginaWeb: PaginaWeb) : RecyclerviewTwo.AdapterTwo<GetViewHolder>(){
 
-    override fun onCreateViewhHolder(parent: ViewGroup): GetViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val cellForRow = layoutInflater.inflate(R.layout.activity_recyclerview_two, parent, false)
-    }
 
-    override fun onBindViewHolder(holder: GetViewHolder,position: Int){
-        val items = paginaWeb.items.get(position)
-        holder.view.autor_Name2.text = items.name
-        holder.view.nome_Completo2.text = items.full_name
-    }
-
-    override fun getItemcoun(): Int {
-        return paginaWeb.items.count()
-    }
 }
--- app/src/main/java/com/example/p11/RecyclerviewTwo.kt
+++ app/src/main/java/com/example/p11/RecyclerviewTwo.kt
@@ -7,18 +7,6 @@ import android.view.ViewGroup
 class RecyclerviewTwo : AppCompatActivity() {
     open class AdapterTwo<T> {
 
-        open fun getItemcoun(): Int {}
-        open fun onCreateViewhHolder(parent: ViewGroup): GetViewHolder {
-            TODO("Not yet implemented")
-        }
-
-        open fun onBindViewHolde(holder: GetViewHolder, position: Int) {}
-        open fun onBindViewHolder() {}
-        open fun onBindViewHolder(holder: GetViewHolder, position: Int) {}
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recyclerview_two)
-    }
 }
